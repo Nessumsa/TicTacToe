46975184d69342f0f24e6d7c400732ff9f816826
Kaqnae <noahvi01@easv365.dk>
2023-12-05 09:35:24 +0100

    Added 2D array

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,11 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    static char[][] gameBoard =
+            {{'_','_','_'},
+            {'_','_','_'},
+            {'_','_','_'}};
+
 
     /**
      * Start of main of TicTacToe
      * @param args
      */
     public static void main(String[] args){
+        printGameBoard();
         //Random comment
+    }
+    private static void printGameBoard(){
+
+        for(char[] row : gameBoard){
+
+          System.out.println(Arrays.toString(row));
+        }
     }
 
 }//End of TicTacToe
