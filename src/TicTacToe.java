2c32ad74adce07ee7ef1a8994bb1630a3a03d2d9
Kaqnae <noahvi01@easv365.dk>
2023-12-05 10:23:16 +0100

    Added new 2D array print

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,19 +8,15 @@ public class TicTacToe {
     private static String player1 = "";
     private static String player2 = "";
     private static String roundPlayer = "";
-    private static int roundNum = 0;
+    private static int roundNum = 1;
 
     static char[][] gameBoard =
-            {{'_','_','_'},
-            {'_','_','_'},
-            {'_','_','_'}};
-    private static void printGameBoard(){
+                    {{'_', '_', '_'},
+                    {'_', '_', '_'},
+                    {'_', '_', '_'}};
 
 
-    /**
-     * Start of main of TicTacToe
-     * @param args
-     */
+
     public static void main(String[] args) {
 
         System.out.print("Welcome to a game of Tic Tac Toe");
@@ -28,13 +24,14 @@ public class TicTacToe {
         player1 = scanner.nextLine();
         System.out.print("\nWhat is the name of player 2: ");
         player2 = scanner.nextLine();
-        System.out.printf("Round 1 is about to start. \n%s will mark the board with X and %s will mark with O. \n%s will begin", player1, player2, player1);
+        System.out.printf("Round 1 is about to start. \n%s will mark the board with X and %s will mark with O. \n%s will begin\n", player1, player2, player1);
 
         printGameBoard();
 
         roundPlay();
 
     }//End void main
+
     private static void roundPlay() {
         if (roundNum % 2 == 0) {
             roundPlayer = player2;
@@ -45,44 +42,30 @@ public class TicTacToe {
         int rowIndex = scanner.nextInt();
         System.out.print("Which column do you choose: ");
         int columnIndex = scanner.nextInt();
-    public static void main(String[] args) {
-        printGameBoard();
-        System.out.print("Welcome to a game of Tic Tac Toe");
-        System.out.print("\nWhat is the name of player 1: ");
-        String player1 = scanner.nextLine();
-        System.out.print("\nWhat is the name of player 2: ");
-        String player2 = scanner.nextLine();
-        System.out.printf("Round 1 is about to start. \n%s will mark the board with X and %s will mark with O. \n%s will begin", player1, player2, player1);
-
-
-    }//End void main
-
-    private static Scanner scanner = new Scanner(System.in);
-        if (roundNum%2 == 1) {
-            gameBoard[rowIndex-1][columnIndex-1] = 'X';
+        if (roundNum % 2 == 1) {
+            gameBoard[rowIndex - 1][columnIndex - 1] = 'X';
+            printGameBoard();
+        } else {
+            gameBoard[rowIndex - 1][columnIndex - 1] = 'O';
             printGameBoard();
         }
-        else{
-            gameBoard[rowIndex-1][columnIndex-1] = 'O';
-            printGameBoard();
-        }
-        if (roundNum >= 6){
+        if (roundNum >= 6) {
             System.out.print("The game is over");
             System.exit(0);
         }
-        roundNum ++;
+        roundNum++;
         roundPlay();
 
-    private static void printGameBoard(){
+
+    }//End roundPlay
+
+    private static void printGameBoard() {
         for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j <gameBoard[i].length ; j++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
                 System.out.print(gameBoard[i][j] + " ");
             }
             System.out.println();
-    }//End roundPlay
-
         }
 
-    }
-
-}//End of TicTacToe
+    }//End of TicTacToe
+}
